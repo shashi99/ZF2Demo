4b971726bc7d1635c54571ba62d96e36929cb818
shashi99 <shashidhar.navalagatti@costrategix.com>
2016-08-25 12:09:25 +0530

    testing

--- module/Application/Controller/AccountController.php
+++ module/Application/Controller/AccountController.php
@@ -18,6 +18,7 @@ class AccountController extends AbstractAppController
     public function indexAction()
     {
         return $this->redirect('login');
+
     }
 
     public function loginAction()
